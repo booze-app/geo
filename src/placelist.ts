3d3981971029202d715e49f52d9a71ec5a5785d5
Justin <justindroegemeier@outlook.de>
2019-11-26 00:28:44 +0000

    contains array of places and provice some functions like ordering

---
+++ src/placelist.ts
@@ -0,0 +1,58 @@
+import { Place } from "place";
+import { NearbySearchResult, PlaceResult } from "googlePlaceTypes";
+import { Pos } from "pos";
+import { Rect } from "rect";
+
+export class PlaceList {
+    public places : Place[]; 
+
+    constructor(places?: Place[]) {
+        if(places)
+            this.places = places;
+        else
+            this.places = [];
+    }
+
+    public convertNearbyPlaceRequest(req: NearbySearchResult, append: boolean = false) : void {
+        if(!this.validateRequest(req))
+            throw Error("There are zero places to convert in the request");        
+        
+        if(!append)
+            this.places = [];
+
+        for(let placeResult of req.results){
+            this.places.push(this.convertPlaceResult(placeResult));
+        }
+    }
+
+    public findShortestPlace(pos: Pos, radius: number) {
+        let rect = Rect.create(pos, radius);
+        let orderedList = this.sortListDistancesShort(pos);
+        for(let place of orderedList) {
+            if(rect.contains(place.location))
+                return place;
+        }
+        return null;
+    }
+
+    public sortListDistancesShort(pos: Pos, override: boolean = false) : Place[] {
+        return this.sortList((a: Place, b: Place) => pos.distance(b.location) - pos.distance(a.location), override);
+    }
+
+    private sortList(sortMethod: (a: Place, b: Place) => number, replace: boolean = false) : Place[] {
+        let sortedList = this.places.sort(sortMethod);
+        if(replace)
+            this.places = sortedList;
+        return sortedList;
+    }
+
+    private convertPlaceResult(place: PlaceResult) : Place {
+        return new Place(place.place_id, place.name, new Pos(place.geometry.location.lat, place.geometry.location.lon));
+    }
+
+    private validateRequest(req: NearbySearchResult) : boolean {
+        if(!req.results || !(req.results.length > 0) )
+            return false;
+        return true;
+    }
+}
