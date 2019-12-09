fc1b9db4237a502a813dbf3d8bb7e7a9eddb91cc
Justin <justindroegemeier@outlook.de>
2019-12-09 13:18:15 +0000

    refactoring

--- src/poslist.ts
+++ src/poslist.ts
@@ -6,7 +6,7 @@ import { Boundaries } from "./boundaries";
  */
 export class PosList {
 
-	public positions: Pos[];
+	readonly positions: Pos[];
 
 	/**
 	 * Creates an instance of PosList
@@ -59,31 +59,7 @@ export class PosList {
      * Search for the point, who is shortest away from the given point
      * @param pos The point, who should used to find the nearest point
      */
-    public findShortestPos(pos: Pos) {
-        return this.sortListDistancesShort(pos)[0];
-    }
-
-    /**
-     * Sort the points from to short to far away given by the point and returns the sorted array
-     * @param pos The point, who is used as indicator
-     * @param override When set true, the list from the instance gets overrided. Default: false
-     */
-    public sortListDistancesShort(pos: Pos, override: boolean = false) : Pos[] {
-        return this.sortList((a: Pos, b: Pos) => pos.distance(a) - pos.distance(b), override);
-    }
-
-    /**
-     * Template function to register new sort methods. It takes a function, who is used in the sort function
-     * @param sortMethod Function, who is used in the sort function   
-     * @param replace When true, the array in the instance gets overriden. Default: false
-     */
-    private sortList(sortMethod: (a: Pos, b: Pos) => number, replace: boolean = false) : Pos[] {
-        let sortedList = this.positions.sort(sortMethod);
-        if(replace)
-        {
-            this.positions = [];
-            this.positions.push(...sortedList);            
-        }
-        return sortedList;
+    public findNearestPos(pos: Pos): Pos {
+        return this.positions.sort((a: Pos, b: Pos) => pos.distance(a) - pos.distance(b))[0];
     }
 }
--- test/poslist.spec.ts
+++ test/poslist.spec.ts
@@ -150,25 +150,6 @@ describe("subset", () => {
 });
 
 describe("order methods", () => {
-
-    test("order short to far away", () => {
-        let p0: Pos = new Pos(0, 0);
-        let p1: Pos = new Pos(1, 1);
-        let p2: Pos = new Pos(2, 2);
-        let p3: Pos = new Pos(3, 3);
-        let p4: Pos = new Pos(4, 4);
-
-        let pa1: Pos[] = [p4, p2, p1, p3];
-        let pl1 = new PosList(pa1);
-        let sortedArray = pl1.sortListDistancesShort(p0, false);
-
-        expect(sortedArray.length).toBe(4);
-        expect(sortedArray[0]).toBe(p1);
-        expect(sortedArray[1]).toBe(p2);
-        expect(sortedArray[2]).toBe(p3);
-        expect(sortedArray[3]).toBe(p4);
-    });
-
     test("find shortest position with given start point", () => {
         let p0: Pos = new Pos(0.123, 0);
         let p1: Pos = new Pos(0.1234, 0);
@@ -179,31 +160,6 @@ describe("order methods", () => {
         let pa1: Pos[] = [p4, p2, p1, p3];
         let pl1 = new PosList(pa1);
        
-        expect(pl1.findShortestPos(p0)).toBe(p1);
-    });
-
-    test("override list in instance", () => {
-        let p0: Pos = new Pos(0.123, 0);
-        let p1: Pos = new Pos(0.1234, 0);
-        let p2: Pos = new Pos(2, 2);
-        let p3: Pos = new Pos(3, 3);
-        let p4: Pos = new Pos(4, 4);
-
-        let pa1: Pos[] = [p4, p2, p1, p3];
-        let pl1 = new PosList(pa1);
-        
-        expect(pl1.positions.length).toBe(4);
-        expect(pl1.positions[0]).toBe(p4);
-        expect(pl1.positions[1]).toBe(p2);
-        expect(pl1.positions[2]).toBe(p1);
-        expect(pl1.positions[3]).toBe(p3);
-
-        pl1.sortListDistancesShort(p0, true);
-
-        expect(pl1.positions.length).toBe(4);
-        expect(pl1.positions[0]).toBe(p1);
-        expect(pl1.positions[1]).toBe(p2);
-        expect(pl1.positions[2]).toBe(p3);
-        expect(pl1.positions[3]).toBe(p4);
+        expect(pl1.findNearestPos(p0)).toBe(p1);
     });
 })
